46931770a9cc183bad2d3da979ca93c3a00faba4
Robin Li <lirobinxc@gmail.com>
2020-12-11 00:04:27 -0500

    packaged nvm

---
+++ src/anki.js
@@ -0,0 +1,13 @@
+// expr1 || expr2
+
+let a = null ?? 'bob'
+
+console.log(a)
+
+// null ?? 'bob'
+// 0 ?? 'bob'
+// undefined ?? 'bob'
+// 'hello' ?? 'bob'
+// false ?? 'bob'
+// '' ?? 'bob'
+// 5 ?? 'bob'
